7c484dd7fa12b1e610de67f1ba5a2c80399725c0
Mohammed Adenwala <adenwala@outlook.com>
2019-02-22 13:44:54 -0600

    Removed credentials.cs file

--- src/Dexcom.Uwp/Credentials.cs
+++
@@ -1,6 +0,0 @@
-﻿public static class Credentials
-{
-    public const string CLIENT_ID = "<INSERT_HERE>";
-    public const string CLIENT_SECRET = "<INSERT_HERE>";
-    public const string CALLBACK_URL = "<INSERT_HERE>";
-}
