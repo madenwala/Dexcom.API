2e3870a8f04a21f914531f9eddfa0b0a6c2dbe69
Mohammed Adenwala <adenwala@outlook.com>
2019-02-22 16:06:17 -0600

    Updated test UI

--- src/Dexcom.Uwp/MainPage.xaml.cs
+++ src/Dexcom.Uwp/MainPage.xaml.cs
@@ -1,11 +1,9 @@
 ﻿using Dexcom.Api;
-using Dexcom.Api.Models;
 using Dexcom.Api.Uwp;
 using Newtonsoft.Json;
 using System;
-using System.Diagnostics;
 using System.Threading;
-using Windows.Security.Authentication.Web;
+using System.Threading.Tasks;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -13,25 +11,63 @@ namespace Dexcom.Uwp
 {
     public sealed partial class MainPage : Page
     {
-        private static DateTime END_DATE = new DateTime(2018, 4, 15);
+        private DexcomClient Client { get; set; }
 
         public MainPage()
         {
             this.InitializeComponent();
+            this.dpStartDate.SelectedDate = new DateTimeOffset(new DateTime(2018, 1, 15));
+            this.dpEndDate.SelectedDate = new DateTimeOffset(new DateTime(2018, 4, 15));
         }
 
-        private async void Button_Click(object sender, RoutedEventArgs e)
+        private async void btnLogin_Click(object sender, RoutedEventArgs e)
         {
             CancellationToken ct = new CancellationToken();
+            await this.AuthenticateAsync(ct);
+            await this.RefreshAsync(ct);
+        }
 
-            var client = new DexcomClient(Credentials.CLIENT_ID, Credentials.CLIENT_SECRET, Credentials.CALLBACK_URL, new DexcomAuthProviderForWindows(), tsIsDev.IsOn);
-            spToken.DataContext = await client.AuthenticateAsync(ct);
-            //client.Token = this.Token;
+        private async void btnLogout_Click(object sender, RoutedEventArgs e)
+        {
+            if (this.Client != null)
+            {
+                CancellationToken ct = new CancellationToken();
+                spToken.DataContext = this.Client = null;
+                await this.RefreshAsync(ct);
+            }
+        }
+
+        private async void btnRefresh_Click(object sender, RoutedEventArgs e)
+        {
+            CancellationToken ct = new CancellationToken();
+            if (this.Client == null)
+                await this.AuthenticateAsync(ct);
+            await this.RefreshAsync(ct);
+        }
+
+        private async Task AuthenticateAsync(CancellationToken ct)
+        {
+            this.Client = new DexcomClient(Credentials.CLIENT_ID, Credentials.CLIENT_SECRET, Credentials.CALLBACK_URL, new DexcomAuthProviderForWindows(), tsIsDev.IsOn);
+            spToken.DataContext = await this.Client.AuthenticateAsync(ct);
+        }
+
+        private async Task RefreshAsync(CancellationToken ct)
+        {
+            if(this.Client?.Token == null)
+            {
+                piDataRange.DataContext = null;
+                piEGV.DataContext = null;
+                piDevices.DataContext = null;
+                piEvents.DataContext = null;
+                piCalibrations.DataContext = null;
+                piStatistics.DataContext = null;
+                return;
+            }
 
             try
             {
                 piDataRange.DataContext = null;
-                piDataRange.DataContext = JsonConvert.SerializeObject(await client.GetDataRangeAsync(ct));
+                piDataRange.DataContext = JsonConvert.SerializeObject(await this.Client.GetDataRangeAsync(ct));
             }
             catch (Exception ex)
             {
@@ -41,9 +77,9 @@ namespace Dexcom.Uwp
             try
             {
                 piEGV.DataContext = null;
-                piEGV.DataContext = JsonConvert.SerializeObject(await client.GetEstimatedGlucoseValueAsync(END_DATE, ct));
+                piEGV.DataContext = JsonConvert.SerializeObject(await this.Client.GetEstimatedGlucoseValueAsync(dpStartDate.SelectedDate.Value.Date, dpEndDate.SelectedDate.Value.Date, ct));
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 piEGV.DataContext = ex.ToString();
             }
@@ -51,7 +87,7 @@ namespace Dexcom.Uwp
             try
             {
                 piDevices.DataContext = null;
-                piDevices.DataContext = JsonConvert.SerializeObject(await client.GetDevicesAsync(END_DATE, ct));
+                piDevices.DataContext = JsonConvert.SerializeObject(await this.Client.GetDevicesAsync(dpStartDate.SelectedDate.Value.Date, dpEndDate.SelectedDate.Value.Date, ct));
             }
             catch (Exception ex)
             {
@@ -61,7 +97,7 @@ namespace Dexcom.Uwp
             try
             {
                 piEvents.DataContext = null;
-                piEvents.DataContext = JsonConvert.SerializeObject(await client.GetEventsAsync(END_DATE, ct));
+                piEvents.DataContext = JsonConvert.SerializeObject(await this.Client.GetEventsAsync(dpStartDate.SelectedDate.Value.Date, dpEndDate.SelectedDate.Value.Date, ct));
             }
             catch (Exception ex)
             {
@@ -71,7 +107,7 @@ namespace Dexcom.Uwp
             try
             {
                 piCalibrations.DataContext = null;
-                piCalibrations.DataContext = JsonConvert.SerializeObject(await client.GetCalibrateAsync(END_DATE, ct));
+                piCalibrations.DataContext = JsonConvert.SerializeObject(await this.Client.GetCalibrateAsync(dpStartDate.SelectedDate.Value.Date, dpEndDate.SelectedDate.Value.Date, ct));
             }
             catch (Exception ex)
             {
@@ -81,7 +117,7 @@ namespace Dexcom.Uwp
             try
             {
                 //Statistics stats = new Statistics();
-                //piStatistics.DataContext = JsonConvert.SerializeObject(await client.PostStatisticsAsync(stats, END_DATE, ct));
+                //piStatistics.DataContext = JsonConvert.SerializeObject(await client.PostStatisticsAsync(stats, dpStartDate.SelectedDate.Value.Date, dpEndDate.SelectedDate.Value.Date, ct));
                 piStatistics.DataContext = "Not yet implemented";
             }
             catch (Exception ex)
